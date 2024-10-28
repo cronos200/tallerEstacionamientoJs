66e80225712136a510d2989885452f47ae08eb20
jhon jairo <jhonjdv2022@outlook.com>
2024-10-27 22:42:27 -0500

    fucion de captura de las horas de entrada y salida

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function inicioSesion() {
                 console.log('Contraseña incorrecta. Intentos restantes: ' + intentos);
                 if (intentos === 0) {
                     console.log('Ha superado el número de intentos permitidos. Bloqueando acceso.');
-                    return false; 
+                    return false;
                 }
             }
         }
@@ -20,4 +20,18 @@ function inicioSesion() {
         console.log('Usuario no existe en la base de datos');
         return false;
     }
+}
+
+function capturarHorasEstacionamiento() {
+    let horaEntrada = parseInt(prompt("Ingrese la hora de entrada (0 a 24):"));
+    let horaSalida = parseInt(prompt("Ingrese la hora de salida (0 a 24):"));
+
+    let horasEstacionado = horaSalida - horaEntrada;
+
+    if (horasEstacionado < 0 || horaEntrada < 0 || horaSalida < 0 || horaEntrada > 24 || horaSalida > 24) {
+        console.log("Horas de entrada o salida inválidas. Intente de nuevo.");
+        return capturarHorasEstacionamiento();
+    }
+
+    return horasEstacionado;
 }
