382450790940959ad3568dbbdb6aa81d5d0f01af
jhon jairo <jhonjdv2022@outlook.com>
2024-10-27 22:34:20 -0500

    creacion de funcion de captura de usuario

---
+++ script.js
@@ -0,0 +1,23 @@
+function inicioSesion() {
+    const usuario = prompt('Ingresa tu usuario:');
+    if (usuario === 'admin') {
+        let intentos = 3;
+        while (intentos > 0) {
+            const contrasena = Number(prompt('Ingrese su contraseña: '));
+            if (contrasena === 123) {
+                console.log('Bienvenido: ' + usuario);
+                return true;
+            } else {
+                intentos--;
+                console.log('Contraseña incorrecta. Intentos restantes: ' + intentos);
+                if (intentos === 0) {
+                    console.log('Ha superado el número de intentos permitidos. Bloqueando acceso.');
+                    return false; 
+                }
+            }
+        }
+    } else {
+        console.log('Usuario no existe en la base de datos');
+        return false;
+    }
+}
